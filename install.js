d5834ca1e24c519ac774b0c79ec164aa13a8b2f5
zhangyunlong <zhangyunlong@baidu.com>
2013-05-11 22:54:05 +0800

    将install功能实现在kernel.util中

--- install.js
+++ install.js
@@ -9,39 +9,19 @@ exports.name = 'install';
 exports.usage = '<name> [options]';
 exports.desc = 'install components or demos';
 exports.register = function(commander){
-    var defaultRepos = fis.config.get(
-        'system.repos',
-        fis.project.DEFAULT_REMOTE_REPOS
-    ).replace(/^\/$/, '') + '/component';
-    
-    function install(name, version, extract, remote){
-        version = version === '*' ? 'latest' : ( version || 'latest' );
-        var url = remote + '/' + name + '/' + version + '.tar';
-        process.stdout.write('install component [' + name + '@' + version + '] ... ');
-        fis.util.download(url, function(err){
-            if(err){
-                process.stdout.write('fail\n');
-                fis.log.error( 'unable to download component [' +
-                    name + '@' + version + '] from [' + url + '], error [' + err + ']');
-            } else {
-                process.stdout.write('ok\n');
-                var pkg = fis.util(extract, 'package.json');
-                if(fis.util.isFile(pkg)){
-                    var info = fis.util.readJSON(pkg);
-                    fis.util.fs.unlinkSync(pkg);
-                    fis.util.map(info.dependencies || {}, function(name, version){
-                        install(name, version, extract, remote);
-                    });
-                }
-            }
-        }, extract);
-    }
     
     commander
-        .option('-r, --repos <url>', 'repository', String, defaultRepos)
+        .option('--repos <url>', 'repository', String)
         .action(function(name, options){
             name = name.split('@');
-            var repos = options.repos.replace(/^\/$/, '');
-            install(name[0], name[1], process.cwd(), repos);
+            var remote = options.repos || fis.config.get(
+                'system.repos',
+                fis.project.DEFAULT_REMOTE_REPOS
+            ).replace(/^\/$/, '') + '/component';
+            var opt = {
+                extract : process.cwd(),
+                remote : remote
+            };
+            fis.util.install(name[0], name[1], opt);
         });
 };
