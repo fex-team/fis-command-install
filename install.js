b2052329151a8996380946eda000494e7b8bf420
zhangyunlong <zhangyunlong@baidu.com>
2013-06-20 20:15:15 +0800

    install支持指定安装路径

--- install.js
+++ install.js
@@ -6,19 +6,29 @@
 'use strict';
 
 exports.name = 'install';
-exports.usage = '<names> [options]';
+exports.usage = '<names> [path] [options]';
 exports.desc = 'install components and demos';
 exports.register = function(commander){
     
     commander
         .option('--repos <url>', 'repository', String)
-        .action(function(names, options){
+        .action(function(){
+            var args = Array.prototype.slice.call(arguments);
+            var options = args.pop();
+            var names = args.shift();
+            var path = args.shift();
             var remote = options.repos || fis.config.get(
                 'system.repos',
                 fis.project.DEFAULT_REMOTE_REPOS
             ).replace(/^\/$/, '') + '/component';
+            var dest = process.cwd();
+            if(path){
+                if(!fis.util.isAbsolute(path)){
+                    dest = fis.util(dest, path);
+                }
+            }
             var opt = {
-                extract : process.cwd(),
+                extract : dest,
                 remote : remote
             };
             names.split(/,/).forEach(function(name){
