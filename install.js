ee8e79cc19653adfdc7730db660051081bc41ebd
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-02 01:07:12 +0800

    optimize

--- install.js
+++ install.js
@@ -28,6 +28,7 @@ exports.register = function(commander) {
             Promise
 
                 // resolve project root.
+                // 根据 fis-conf.js 所在目录来决定 root 是哪个目录。
                 .try(function() {
                     if (!settings.root) {
                         var findup = require('findup');
@@ -55,6 +56,7 @@ exports.register = function(commander) {
                 })
 
                 // load fis-conf.js if exists.
+                // 读取用户配置信息。
                 .then(function() {
                     var path = require('path');
                     var exists = require('fs').existsSync;
@@ -63,11 +65,19 @@ exports.register = function(commander) {
                     if (exists(filepath)) {
                         require(filepath);
 
-                        settings.componentsDir = path.join(settings.root, fis.config.get('componentsDir') || '/components');
+                        // 应用  fis-conf.js
+                        settings.componentsDir = path.join(settings.root,
+                                fis.config.get('component.dir') || '/components');
+
+                        settings.defaultRepos = fis.config.get('component.default', 'github');
+                        settings.github = fis.config.get('component.github', {});
+                        settings.gitlab = fis.config.get('component.gitlab', {});
+                        settings.lights = fis.config.get('component.lights', {});
                     }
                 })
 
                 // Check components.json if did not sepecified any components from command line.
+                // 如果没有指定参数，将通过 components.json 来决定安装什么组件。
                 .then(function() {
                     var components = settings.components;
 
@@ -90,98 +100,125 @@ exports.register = function(commander) {
 
                     var factory = require('./lib/remotes/factory.js');
 
+                    factory.setSettings(settings);
+
                     // validate and filter invalid dependencies.
-                    components = components
+                    // 过滤掉不能识别的依赖。
+                    return components
                         .map(function(component) {
                             var type = factory.detect(component);
 
                             if (!type) {
-                                logger.warn('`%s` is not a valid dependency.', component);
+                                logger.warn('`%s` is not a valid repository.', component);
                                 return null;
                             }
 
-                            return factory(type)({
-                                address: component,
-                                settings: settings
-                            });
+                            return factory(component);
                         })
                         .filter(function(item) {
                             return item != null;
                         });
+                })
 
+                .then(function(components) {
                     if (!components.length) {
                         logger.warn('Installed nothing!');
                         return;
                     }
 
                     var collector = require('./lib/collector.js');
-                    return collector(components);
-                })
-
-                .then(function(components) {
-
-                    // 过滤掉本地的仓库。
-                    components = components.filter(function(item) {
-                        return item.location !== 'local';
-                    });
-
-                    if (!components.length) {
-                        console.log('Aready installed');
-                        return;
-                    }
-
-                    var ProgressBar = require('progress');
-                    var percentages = {};
-                    var bar, ticked;
-                    var update = function(name, loaded, total) {
-                        percentages[name] = percentages[name] || {};
-                        percentages[name].loaded = loaded;
-                        percentages[name].total = total;
-                        updateAll();
-                    };
-                    var updateAll = function() {
-                        bar = bar || new ProgressBar(' download [:bar] :percent :etas', {
-                            incomplete: ' ',
-                            total: 100,
-                            clear: true
-                        });
-
-                        var total = 0;
-                        var loaded = 0;
-
-                        Object.keys(percentages).forEach(function(key) {
-                            var item = percentages[key];
-
-                            total += item.total;
-                            loaded += item.loaded;
-                        });
-
-                        var percentage = Math.round(loaded * 100 / total);
-                        var intervel = percentage - ticked;
-                        ticked = percentage;
-                        bar.tick(Math.max(intervel, 0));
-                    }
-
-                    return Promise
-
-                        .all(components.map(function(component) {
-                            return component.install(function(percent, loaded, total) {
-                                update(component.name, loaded, total);
-                            });
-                        }))
+                    return collector(components)
 
                         .then(function(components) {
-                            var last = components.length - 1;
-                            var arrs = components.map(function(item, index) {
-                                return (index === last ? '└── ' : '├── ') + item.address + '@' + item.version;
-                            });
+                            var _ = require('./lib/util.js');
 
-                            console.log('Installed\n%s', arrs.join('\n'));
+                            // 过滤掉本地的仓库。已经同名的包。
+                            components = components
+
+                                .filter(function(item) {
+                                    return item.location !== 'local';
+                                })
+
+                                // 先不 sort 了，通过先后顺序来准定用哪个版本吧。
+                                // 而不是总是用最新版本。
+                                // .sort(function(a, b) {
+                                //     return _.compareVersion(a.version, b.version);
+                                // })
+
+                                // 过滤同名的组件名。
+                                .filter(function(item, index, list) {
+                                    return _.indexOfArray('name', item.name, list) === index;
+                                });
+
+                            if (!components.length) {
+                                console.log('Aready installed');
+                                return;
+                            }
+
+                            var ProgressBar = require('progress');
+                            var percentages = {};
+                            var bar;
+                            var ticked = 0;
+                            var update = function(name, loaded, total) {
+                                percentages[name] = percentages[name] || {};
+                                percentages[name].loaded = loaded;
+                                percentages[name].total = total;
+                                updateAll();
+                            };
+                            var updateAll = function() {
+                                bar = bar || new ProgressBar(' downloading [:bar] :percent :etas', {
+                                    incomplete: ' ',
+                                    total: 100,
+                                    clear: true
+                                });
+
+                                var total = 0;
+                                var loaded = 0;
+
+                                Object.keys(percentages).forEach(function(key) {
+                                    var item = percentages[key];
+
+                                    total += item.total;
+                                    loaded += item.loaded;
+                                });
+
+                                var percentage = Math.round(loaded * 100 / total);
+                                var step = Math.max(percentage - ticked, 0);
+                                ticked += step;
+                                bar.tick(step);
+                            }
+
+                            return Promise
+
+                                .all(components.map(function(component) {
+                                    return component.install(function(percent, loaded, total) {
+                                        update(component.name, loaded, total);
+                                    });
+                                }))
+
+                                .then(function(components) {
+
+                                    // 如果没有 tick 完，则收下尾
+                                    (100 - ticked) && bar && bar.tick(100 - ticked);
+
+                                    var last = components.length - 1;
+                                    var arrs = components.map(function(item, index) {
+                                        return (index === last ? '└── ' : '├── ') + item.address + '@' + item.version;
+                                    });
+
+                                    console.log('Installed\n%s', arrs.join('\n'));
+                                });
                         });
+                })
+
+                // 保存 components.json
+                .then(function() {
+                    // todo
+                    // 如果指定了  --save， 则需要把数据写入到 components.josn 文件里面。
                 })
 
                 // error handle
-                .error(function(e) {
+                .catch(function(e) {
                     logger.error('\x1b[31m%s\x1b[0m', e.message);
                 });
         });
